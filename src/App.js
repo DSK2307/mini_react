c417ab87e745ea582a966fef515408c73ae9fc5c
Dsk <deosagar150@gmail.com>
2024-12-01 01:59:38 +0530

    This is a new commit for what I originally planned to be amended

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className=" area w-full h-screen flex flex-col items-center justify-center bg-gray-100">
+        
+      <div className="flex items-center justify-center group rounded text-sm font-semibold cursor-pointer hover:bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 w-fit">
+          <div className="group-hover:bg-black bg-transparent w-fit h-full p-1.5">
+            <h1 className="font-extrabold text-transparent text-3xl sm:text-4xl xl:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-transparent h-full">
+            <div className="text-xl font-semibold mb-8">
+          <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+          <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-row gap-4">
+          <div>
+            {running ? (
+              <button
+                className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 px-4 py-2"
+                onClick={() => {
+                  setRunning(false);
+                }}
+              >
+                Stop
+              </button>
+            ) : (
+              <button
+                className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 px-4 py-2"
+                onClick={() => {
+                  setRunning(true);
+                }}
+              >
+                Start
+              </button>
+            )}
+          </div>
+          <button
+            className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 px-4 py-2"
+            onClick={() => {
+              setTime(0);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
